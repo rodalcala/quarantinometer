c01a206bd46c500eb08134000715b7769daa68d4
Rodrigo Alcala <alcala@myego2go.de>
2020-03-30 22:45:13 +0200

    chore(DateInput): limit the date range to COVID-19 times

--- components/DateInput.js
+++ components/DateInput.js
@@ -3,7 +3,15 @@ import DatePicker from 'react-datepicker';
 const DateInput = ({ startDate, setStartDate }) => (
   <div className='container'>
     <h3>When did your quarantine began?</h3>
-    <DatePicker inline dateFormat="dd/MM/yyyy" selected={startDate} onChange={date => setStartDate(date)} />
+    <DatePicker
+      inline
+      dateFormat="dd/MM/yyyy"
+      selected={startDate}
+      onChange={date => setStartDate(date)}
+      minDate={new Date('Wed Jan 01 2020')}
+      maxDate={new Date()}
+      showDisabledMonthNavigation
+    />
     <style jsx>{`
       .container {
         padding: 0 0.5rem;
