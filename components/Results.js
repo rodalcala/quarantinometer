5cdfd00e1ae6a4dafcd23eeda373834c7cc52dc2
Rodrigo Alcala <alcala@myego2go.de>
2020-03-30 22:33:26 +0200

    feat(Results): dynamically render the component according to the number of elapsedDays

--- components/Results.js
+++ components/Results.js
@@ -1,17 +1,24 @@
-const Results = ({ elapsedDays }) => (
-  <div className='container'>
-    <h3>You've been in quarantine for</h3>
-    <h1>{ elapsedDays + ' days' }</h1>
-    <style jsx>{`
-      .container {
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-    `}</style>
-  </div>
-);
+const Results = ({ elapsedDays }) => {
+  /** NOTE: In case the user didn't submit a starting date, don't render the component */
+  if (!elapsedDays) return null;
+
+  const elapsedDaysText = elapsedDays + (elapsedDays === 1 ? ' day' : ' days');
+
+  return (
+    <div className='container'>
+      <h3>You've been in quarantine for</h3>
+      <h1>{ elapsedDaysText }</h1>
+      <style jsx>{`
+        .container {
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+      `}</style>
+    </div>
+  )
+};
 
 export default Results;
